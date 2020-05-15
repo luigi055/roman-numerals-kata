82d6dad28831a0203fa4574bb05a641d41230760
Pedro La Rosa <pedrolarosad@gmail.com>
2020-05-15 10:50:10 +0200

    adding romanNumerals class

--- RomanNumerals/RomanNumerals.cs
+++ RomanNumerals/RomanNumerals.cs
@@ -2,7 +2,23 @@
 
 namespace RomanNumerals
 {
+
+
   public class RomanNumerals
   {
+    private const string OneRoman = "I";
+
+    public string Convert(int number)
+    {
+
+      if (number == 0) return "";
+      if (number == 4)
+      {
+
+        return "IV";
+      }
+
+      return OneRoman + Convert(number - 1);
+    }
   }
 }
