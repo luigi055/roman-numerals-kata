ec67daec4c69b66c02a5eb66ad6d55ee454346f4
Pedro La Rosa <pedrolarosad@gmail.com>
2020-05-15 10:46:59 +0200

    can convert numbers from I to IV

--- RomanNumerals.Test.Unit/RomanNumeralsShould.cs
+++ RomanNumerals.Test.Unit/RomanNumeralsShould.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using RomanNumerals;
 
 namespace RomanNumerals.Test.Unit
 {
@@ -8,7 +9,39 @@ namespace RomanNumerals.Test.Unit
     [Fact]
     public void ConvertOneToI()
     {
+      var romanNumerals = new RomanNumerals();
 
+      var converted = romanNumerals.Convert(1);
+
+      Assert.True(converted == "I");
+    }
+    [Fact]
+    public void ConvertTwoToII()
+    {
+      var romanNumerals = new RomanNumerals();
+
+      var converted = romanNumerals.Convert(2);
+
+      Assert.True(converted == "II");
+    }
+    [Fact]
+    public void ConvertThreeToIII()
+    {
+      var romanNumerals = new RomanNumerals();
+
+      var converted = romanNumerals.Convert(3);
+
+      Assert.True(converted == "III");
+    }
+
+    [Fact]
+    public void ConvertFourToIV()
+    {
+      var romanNumerals = new RomanNumerals();
+
+      var converted = romanNumerals.Convert(4);
+
+      Assert.True(converted == "IV");
     }
   }
 }
